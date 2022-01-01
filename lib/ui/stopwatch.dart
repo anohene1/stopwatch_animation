c884e4b96647c64e395d7c94b66015d768cdb537
anohene1 <anoheneisaac@gmail.co,m>
2022-01-01 10:22:30 +0000

    initial commit

---
+++ lib/ui/stopwatch.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
+
+import 'elapsed_time_text.dart';
+
+class Stopwatch extends StatefulWidget {
+  @override
+  _StopwatchState createState() => _StopwatchState();
+}
+
+class _StopwatchState extends State<Stopwatch> with SingleTickerProviderStateMixin {
+  Duration _elapsed = Duration.zero;
+  late final Ticker _ticker;
+
+  @override
+  void initState() {
+    _ticker = this.createTicker((elapsed) {
+      setState(() {
+        _elapsed = elapsed;
+      });
+    });
+    _ticker.start();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElapsedTimeText(
+      elapsed: _elapsed,
+    );
+  }
+}
